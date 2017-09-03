ddbe5ac4c790b3381b655375534a442a85609676
allthesignals <wilburnforce@gmail.com>
2017-09-03 19:40:29 -0400

    Only load mapboxgl if it’s in the viewport

---
+++ app/components/mapbox-gl.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import MapboxGl from 'ember-mapbox-gl/components/mapbox-gl';
+import InViewportMixin from 'ember-in-viewport';
+
+export default MapboxGl.extend(InViewportMixin, {
+});
